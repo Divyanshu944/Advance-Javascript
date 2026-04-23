1a7ab8b796ea079cf91db0e31fab2d62355273c9
Divyanshu pal <paldivyanshu944@gmail.com>
2026-04-23 12:13:35 -0400

    live counter

--- Live character counter/script.js
+++ Live character counter/script.js
@@ -1,6 +1,23 @@
-let inp = document.querySelector("input");
-let spa = document.querySelector("span")
+// For Increasing..
 
-inp.addEventListener("input", function(){
-    spa.textContent = inp.value.length
+// let inp = document.querySelector("input");
+// let spa = document.querySelector("span")
+
+// inp.addEventListener("input", function(){
+//     spa.textContent = inp.value.length
+// })
+
+// For decreasing..
+
+let input = document.querySelector("input")
+let span = document.querySelector("span")
+
+input.addEventListener("input", function(){
+    let left = 20 - input.value.length;
+    span.textContent = left;
+    if(left < 0){
+        span.style.color = "red";
+    }else{
+        span.style.color = "white";
+    }
 })
