4c0adfdbbdb76d3802d9919990293bba4a4d83bd
Divyanshu pal <paldivyanshu944@gmail.com>
2026-05-06 17:21:43 -0400

    aced

--- settimout/script.js
+++ settimout/script.js
@@ -1,9 +1,15 @@
-let count = 12;
+let count = 0;
+let second = 5;
+let process = document.querySelector(".progress-bar");
+let percentagetext = document.querySelector(".percentage")
 
-let interval = setInterval(() => {
-    if(count >= 1){
-        count--;
-        console.log(count);
-        
-    }else clearInterval(interval)
-}, 1000);
+let intv = setInterval(()=>{
+if(count <= 99){
+    count++;
+    process.style.width = `${count}%`
+    percentagetext.textContent = `${count}%`
+}else{
+    document.querySelector(".status").textContent="Downloaded."
+    clearInterval(intv)
+}
+},(second*1000)/100)
